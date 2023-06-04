75ac96cf50b9b6928cde5ac3bb76cc3f7a08c0c6
mohaelmi <moha.abdi1771@gmail.com>
2023-06-04 15:56:08 -0500

    added sum recursion function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if(fromN === toN) return toN
+  
+  return fromN + sum(fromN+1, toN)
 }
+
+
+//3 + 4 + 5 + 6 + 7
+//3 + 22 = 25
+//4 + 18 = 22
+//5 + 13 = 18
+//6 + 7 = 13
+//sum(7, 7) return 7
+
+console.log(sum(3,7))
 
 module.exports = sum;
